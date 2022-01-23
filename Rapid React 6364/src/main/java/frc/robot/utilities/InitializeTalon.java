457db0cf031f33f6180387ae4722f4d419bb7601
Jacob Chisholm <Jacobchisholm1010@gmail.com>
2022-01-22 22:57:22 -0800

    Update InitializeTalon.java

--- Rapid React 6364/src/main/java/frc/robot/utilities/InitializeTalon.java
+++ Rapid React 6364/src/main/java/frc/robot/utilities/InitializeTalon.java
@@ -1,13 +1,15 @@
 package frc.robot.utilities;
 
 import com.ctre.phoenix.motorcontrol.FeedbackDevice;
+import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.can.TalonFX;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 
 import frc.robot.Constants;
 
 public class InitializeTalon {
 
-    public static void Generic(final TalonFX iMotor, boolean inverted){
+    public static void GenericFX(final TalonFX iMotor, boolean inverted){
         iMotor.configSelectedFeedbackSensor(FeedbackDevice.IntegratedSensor); //Set to use Internal Encoder
         iMotor.setSensorPhase(false); //Disable Sensor Phase (tuning)
         iMotor.setInverted(inverted); //Set Motor Inverted
@@ -19,6 +21,17 @@ public class InitializeTalon {
         iMotor.configPeakOutputReverse(-1);
 
         iMotor.setSelectedSensorPosition(0); // Reset Motor Encoder
+    }
+
+    public static void GenericSRX(final TalonSRX iMotor, boolean inverted){
+        iMotor.configSelectedFeedbackSensor(FeedbackDevice.None); //Set Encoder
+        iMotor.setInverted(inverted); //Set Motor Inverted
+        iMotor.configFactoryDefault(); //Use Factory Tune Settings
+        //Output Settings
+        iMotor.configNominalOutputForward(0);
+        iMotor.configNominalOutputReverse(0);
+        iMotor.configPeakOutputForward(1);
+        iMotor.configPeakOutputReverse(-1);
     }
     
     public static void RightDrive(final TalonFX iMotor){
@@ -59,9 +72,8 @@ public class InitializeTalon {
         iMotor.setSelectedSensorPosition(0); // Reset Motor Encoder       
     }
 
-    public static void Intake(final TalonFX iMotor, boolean inverted){
-        iMotor.configSelectedFeedbackSensor(FeedbackDevice.IntegratedSensor); //Set to use Internal Encoder
-        iMotor.setSensorPhase(false); //Disable Sensor Phase (tuning)
+    public static void Intake(final TalonSRX iMotor, boolean inverted){
+        iMotor.configSelectedFeedbackSensor(FeedbackDevice.None); //Set Encoder
         iMotor.setInverted(inverted); //Set Motor Invert
         iMotor.configFactoryDefault(); //Use Factory Tune Settings
         //Output Settings
@@ -73,14 +85,11 @@ public class InitializeTalon {
         iMotor.config_kF(Constants.kPIDLoopIdx, Constants.Intake.mkF, Constants.kTimeoutMs);
         iMotor.config_kP(Constants.kPIDLoopIdx, Constants.Intake.mkP, Constants.kTimeoutMs);
         iMotor.config_kI(Constants.kPIDLoopIdx, Constants.Intake.mkI, Constants.kTimeoutMs);
-        iMotor.config_kD(Constants.kPIDLoopIdx, Constants.Intake.mkD, Constants.kTimeoutMs);
-
-        iMotor.setSelectedSensorPosition(0); // Reset Motor Encoder       
+        iMotor.config_kD(Constants.kPIDLoopIdx, Constants.Intake.mkD, Constants.kTimeoutMs);  
     }
 
-    public static void Index(final TalonFX iMotor, boolean inverted){
-        iMotor.configSelectedFeedbackSensor(FeedbackDevice.IntegratedSensor); //Set to use Internal Encoder
-        iMotor.setSensorPhase(false); //Disable Sensor Phase (tuning)
+    public static void Index(final TalonSRX iMotor, boolean inverted){
+        iMotor.configSelectedFeedbackSensor(FeedbackDevice.SoftwareEmulatedSensor); //Set Encoder
         iMotor.setInverted(inverted); //Set Motor Invert
         iMotor.configFactoryDefault(); //Use Factory Tune Settings
         //Output Settings
@@ -94,6 +103,8 @@ public class InitializeTalon {
         iMotor.config_kI(Constants.kPIDLoopIdx, Constants.Index.mkI, Constants.kTimeoutMs);
         iMotor.config_kD(Constants.kPIDLoopIdx, Constants.Index.mkD, Constants.kTimeoutMs);
 
-        iMotor.setSelectedSensorPosition(0); // Reset Motor Encoder       
+        iMotor.setNeutralMode(NeutralMode.Brake); // Set Motor Neutral Mode To Hold
+
+        //iMotor.setSelectedSensorPosition(0); // Reset Motor Encoder
     }
 }
