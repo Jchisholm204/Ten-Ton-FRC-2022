068eff35305bd88071eb5de7e3eb095b3ea3455d
Jacob Chisholm <jacobchisholm1010@gmail.com>
2022-02-11 09:48:49 -0800

    Update All motors to use new type of configuration

--- Rapid React 6364/src/main/java/frc/robot/utilities/Talon.java
+++ Rapid React 6364/src/main/java/frc/robot/utilities/Talon.java
@@ -6,152 +6,160 @@ import com.ctre.phoenix.motorcontrol.can.TalonFX;
 import com.ctre.phoenix.motorcontrol.can.TalonFXConfiguration;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 
+import com.ctre.phoenix.motorcontrol.can.TalonSRXConfiguration;
 import frc.robot.Constants;
 
 public class Talon {
 
     public static class Initialize {
 
-        public static void GenericFX(final TalonFX iMotor, boolean inverted) {
-            iMotor.configSelectedFeedbackSensor(FeedbackDevice.IntegratedSensor); //Set to use Internal Encoder
-            iMotor.setSensorPhase(false); //Disable Sensor Phase (tuning)
-            iMotor.setInverted(inverted); //Set Motor Inverted
-            iMotor.configFactoryDefault(); //Use Factory Tune Settings
-            //Output Settings
-            iMotor.configNominalOutputForward(0);
-            iMotor.configNominalOutputReverse(0);
-            iMotor.configPeakOutputForward(1);
-            iMotor.configPeakOutputReverse(-1);
+        private static TalonFXConfiguration DriveConfiguration(double feedForward){
+            TalonFXConfiguration configuration = new TalonFXConfiguration();
 
-            iMotor.setSelectedSensorPosition(0); // Reset Motor Encoder
+            configuration.primaryPID.selectedFeedbackSensor = FeedbackDevice.IntegratedSensor; //TalonFX uses Integrated Motor Sensor
+            configuration.slot0.kP = 0.0; // Standard PID kP - used for MagicMotion movement
+            configuration.slot0.kI = 0.0; // Standard PID kI - used for MagicMotion movement
+            configuration.slot0.kD = 0.0; // Standard PID kD - used for MagicMotion movement
+            configuration.slot0.kF = feedForward; // kF - Feed Forward Value - The minimum motor power you have to apply to move the motor
+
+            configuration.slot0.integralZone = 0;
+            configuration.slot0.allowableClosedloopError = 0;
+            configuration.closedloopRamp = 0;
+
+            configuration.motionAcceleration = 2000; // Maximum Acceleration for MagicMotion
+            configuration.motionCruiseVelocity = 22000/*fs*/; // Maximum Speed (in encoder units / 100ms) for Magic Motion
+
+            configuration.nominalOutputForward = 0.0;
+            configuration.nominalOutputReverse = 0.0;
+            configuration.peakOutputForward = 1.0;
+            configuration.peakOutputReverse = -1.0;
+
+            configuration.motionCurveStrength = 3; // Magic Motion Curve Strength (read the docs)
+
+            return configuration; //Return the Motor Configuration
         }
 
-        public static void GenericSRX(final TalonSRX iMotor, boolean inverted) {
-            iMotor.configSelectedFeedbackSensor(FeedbackDevice.None); //Set Encoder
-            iMotor.setInverted(inverted); //Set Motor Inverted
-            iMotor.configFactoryDefault(); //Use Factory Tune Settings
-            //Output Settings
-            iMotor.configNominalOutputForward(0);
-            iMotor.configNominalOutputReverse(0);
-            iMotor.configPeakOutputForward(1);
-            iMotor.configPeakOutputReverse(-1);
+        private static TalonFXConfiguration WinchConfiguration(){
+
+            TalonFXConfiguration configuration = new TalonFXConfiguration(); //Set up new motor configuration
+
+            configuration.primaryPID.selectedFeedbackSensor = FeedbackDevice.IntegratedSensor; //TalonFX uses Integrated Motor Sensor
+            configuration.slot0.kP = 2.0; // Standard PID kP - used for MagicMotion movement
+            configuration.slot0.kI = 0.0; // Standard PID kI - used for MagicMotion movement
+            configuration.slot0.kD = 80.0; // Standard PID kD - used for MagicMotion movement
+            configuration.slot0.kF = 0.0505; // kF - Feed Forward Value - The minimum motor power you have to apply to move the motor
+
+            configuration.slot0.integralZone = 0;
+            configuration.slot0.allowableClosedloopError = 0;
+            configuration.closedloopRamp = 0;
+
+            configuration.motionAcceleration = 2000; // Maximum Acceleration for MagicMotion
+            configuration.motionCruiseVelocity = 22000/*fs*/ / 4 /*quarter speed*/; // Maximum Speed (in encoder units / 100ms) for Magic Motion
+
+            configuration.nominalOutputForward = 0.0;
+            configuration.nominalOutputReverse = 0.0;
+            configuration.peakOutputForward = 1.0;
+            configuration.peakOutputReverse = -1.0;
+
+            configuration.motionCurveStrength = 3; // Magic Motion Curve Strength (read the docs)
+
+            return configuration; //Return the Motor Configuration
+        }
+
+        private static TalonSRXConfiguration IntakeConfiguration(){
+            TalonSRXConfiguration configuration = new TalonSRXConfiguration();
+
+            configuration.primaryPID.selectedFeedbackSensor = FeedbackDevice.None;
+            configuration.slot0.kP = 0.0; // Standard PID kP - used for MagicMotion movement
+            configuration.slot0.kI = 0.0; // Standard PID kI - used for MagicMotion movement
+            configuration.slot0.kD = 0.0; // Standard PID kD - used for MagicMotion movement
+            configuration.slot0.kF = 0.0505; // kF - Feed Forward Value - The minimum motor power you have to apply to move the motor
+
+            configuration.slot0.integralZone = 0;
+            configuration.slot0.allowableClosedloopError = 0;
+            configuration.closedloopRamp = 0;
+
+            configuration.motionAcceleration = 2000; // Maximum Acceleration for MagicMotion
+            configuration.motionCruiseVelocity = 22000/*fs*/; // Maximum Speed (in encoder units / 100ms) for Magic Motion
+
+            configuration.nominalOutputForward = 0.0;
+            configuration.nominalOutputReverse = 0.0;
+            configuration.peakOutputForward = 1.0;
+            configuration.peakOutputReverse = -1.0;
+
+            configuration.motionCurveStrength = 3; // Magic Motion Curve Strength (read the docs)
+
+            return configuration; //Return the Motor Configuration
+        }
+
+        private static TalonSRXConfiguration IndexConfiguration(){
+            TalonSRXConfiguration configuration = new TalonSRXConfiguration();
+
+            configuration.primaryPID.selectedFeedbackSensor = FeedbackDevice.None;
+            configuration.slot0.kP = 0.0; // Standard PID kP - used for MagicMotion movement
+            configuration.slot0.kI = 0.0; // Standard PID kI - used for MagicMotion movement
+            configuration.slot0.kD = 0.0; // Standard PID kD - used for MagicMotion movement
+            configuration.slot0.kF = 0.0505; // kF - Feed Forward Value - The minimum motor power you have to apply to move the motor
+
+            configuration.slot0.integralZone = 0;
+            configuration.slot0.allowableClosedloopError = 0;
+            configuration.closedloopRamp = 0;
+
+            configuration.motionAcceleration = 2000; // Maximum Acceleration for MagicMotion
+            configuration.motionCruiseVelocity = 22000/*fs*/; // Maximum Speed (in encoder units / 100ms) for Magic Motion
+
+            configuration.nominalOutputForward = 0.0;
+            configuration.nominalOutputReverse = 0.0;
+            configuration.peakOutputForward = 1.0;
+            configuration.peakOutputReverse = -1.0;
+
+            configuration.motionCurveStrength = 3; // Magic Motion Curve Strength (read the docs)
+
+            return configuration; //Return the Motor Configuration
         }
 
         public static void RightDrive(final TalonFX iMotor) {
-            iMotor.configSelectedFeedbackSensor(FeedbackDevice.IntegratedSensor); //Set to use Internal Encoder
-            iMotor.setSensorPhase(false); //Disable Sensor Phase (tuning)
-            iMotor.setInverted(true); //Invert Motor
-            iMotor.configFactoryDefault(); //Use Factory Tune Settings
-            //Output Settings
-            iMotor.configNominalOutputForward(0);
-            iMotor.configNominalOutputReverse(0);
-            iMotor.configPeakOutputForward(1);
-            iMotor.configPeakOutputReverse(-1);
-
-            iMotor.config_kF(Constants.kPIDLoopIdx, Constants.Drive.RightkF, Constants.kTimeoutMs);
-            iMotor.config_kP(Constants.kPIDLoopIdx, Constants.Drive.RightkP, Constants.kTimeoutMs);
-            iMotor.config_kI(Constants.kPIDLoopIdx, Constants.Drive.RightkI, Constants.kTimeoutMs);
-            iMotor.config_kD(Constants.kPIDLoopIdx, Constants.Drive.RightkD, Constants.kTimeoutMs);
+            iMotor.configFactoryDefault(); // Set to Factory Tune Before Applying any kind of user Configuration
+            iMotor.configAllSettings(DriveConfiguration(0.050));
+            iMotor.setInverted(true); // Invert Motor
 
             iMotor.setSelectedSensorPosition(0); // Reset Motor Encoder
         }
 
         public static void LeftDrive(final TalonFX iMotor) {
-            iMotor.configSelectedFeedbackSensor(FeedbackDevice.IntegratedSensor); //Set to use Internal Encoder
-            iMotor.setSensorPhase(false); //Disable Sensor Phase (tuning)
-            iMotor.setInverted(false); //Do Not Invert Motor
-            iMotor.configFactoryDefault(); //Use Factory Tune Settings
-            //Output Settings
-            iMotor.configNominalOutputForward(0);
-            iMotor.configNominalOutputReverse(0);
-            iMotor.configPeakOutputForward(1);
-            iMotor.configPeakOutputReverse(-1);
-
-            iMotor.config_kF(Constants.kPIDLoopIdx, Constants.Drive.LeftkF, Constants.kTimeoutMs);
-            iMotor.config_kP(Constants.kPIDLoopIdx, Constants.Drive.LeftkP, Constants.kTimeoutMs);
-            iMotor.config_kI(Constants.kPIDLoopIdx, Constants.Drive.LeftkI, Constants.kTimeoutMs);
-            iMotor.config_kD(Constants.kPIDLoopIdx, Constants.Drive.LeftkD, Constants.kTimeoutMs);
+            iMotor.configFactoryDefault(); // Set to Factory Tune Before Applying any kind of user Configuration
+            iMotor.configAllSettings(DriveConfiguration(0.050));
+            iMotor.setInverted(false); // Don't Invert Motor
 
             iMotor.setSelectedSensorPosition(0); // Reset Motor Encoder
         }
 
-        public static void Hang(final TalonFX iMotor, boolean inverted) {
-            iMotor.configSelectedFeedbackSensor(FeedbackDevice.IntegratedSensor); //Set to use Internal Encoder
-            iMotor.setSensorPhase(false); // Disable Sensor Phase (tuning)
-            iMotor.setInverted(inverted); // Invert Motor?
-            iMotor.configFactoryDefault(); // Use Factory Tune Settings
-            //Output Settings
-            iMotor.configNominalOutputForward(0);
-            iMotor.configNominalOutputReverse(0);
-            iMotor.configPeakOutputForward(1);
-            iMotor.configPeakOutputReverse(-1);
-
-            iMotor.config_kF(Constants.kPIDLoopIdx, 0.0500, Constants.kTimeoutMs);
-            iMotor.config_kP(Constants.kPIDLoopIdx, 0.25, Constants.kTimeoutMs);
-            iMotor.config_kI(Constants.kPIDLoopIdx, 0, Constants.kTimeoutMs);
-            iMotor.config_kD(Constants.kPIDLoopIdx, 0, Constants.kTimeoutMs);
-        }
-
         public static void Winch(final TalonFX iMotor, boolean inverted) {
-            TalonFXConfiguration winchConfig = new TalonFXConfiguration(); //Create Motor Configuration
-            winchConfig.primaryPID.selectedFeedbackSensor = FeedbackDevice.IntegratedSensor; //Set to use integrated Sensor
-            winchConfig.slot0.kP = 2;
-            winchConfig.slot0.kI = 0.0;
-            winchConfig.slot0.kD =80.0;
-            winchConfig.slot0.kF = 0.0505;
-            winchConfig.closedloopRamp = 1;
-            winchConfig.slot0.integralZone = 0;
-            winchConfig.slot0.allowableClosedloopError = 0;
-            winchConfig.motionAcceleration = 2000; //1000
-            winchConfig.motionCruiseVelocity = 22000/*fs*//2; // to get half speed
-            winchConfig.nominalOutputForward = 0.0;
-            winchConfig.nominalOutputReverse = 0.0;
-            winchConfig.peakOutputReverse = -1;
-            winchConfig.peakOutputForward = 1;
-            winchConfig.motionCurveStrength = 3;
 
-            iMotor.configFactoryDefault(); //Reset Motor Configuration
-            iMotor.configAllSettings(winchConfig); //Apply new Motor Configuration
+            iMotor.configFactoryDefault(); //Reset Motor Configuration before applying any kind of user configuration
+            iMotor.configAllSettings(WinchConfiguration()); //Apply new Motor Configuration
 
-            //iMotor.setSensorPhase(true); // Sensor Phase (tuning)
+            //iMotor.setSensorPhase(true); // Sensor Phase (tuning) // Not needed for TalonFX Integrated Sensors
             iMotor.setInverted(inverted); // Invert Motor?
 
         }
 
         public static void Intake(final TalonSRX iMotor, boolean inverted) {
-            iMotor.configSelectedFeedbackSensor(FeedbackDevice.None); //Set Encoder
-            iMotor.setInverted(inverted); //Set Motor Invert
-            iMotor.configFactoryDefault(); //Use Factory Tune Settings
-            //Output Settings
-            iMotor.configNominalOutputForward(0);
-            iMotor.configNominalOutputReverse(0);
-            iMotor.configPeakOutputForward(1);
-            iMotor.configPeakOutputReverse(-1);
 
-            iMotor.config_kF(Constants.kPIDLoopIdx, Constants.Intake.mkF, Constants.kTimeoutMs);
-            iMotor.config_kP(Constants.kPIDLoopIdx, Constants.Intake.mkP, Constants.kTimeoutMs);
-            iMotor.config_kI(Constants.kPIDLoopIdx, Constants.Intake.mkI, Constants.kTimeoutMs);
-            iMotor.config_kD(Constants.kPIDLoopIdx, Constants.Intake.mkD, Constants.kTimeoutMs);
+            iMotor.configFactoryDefault(); //Reset Motor Configuration before applying any kind of user configuration
+            iMotor.configAllSettings(IntakeConfiguration()); //Apply new Motor Configuration
+
+            //iMotor.setSensorPhase(true); // Sensor Phase (tuning) // Not needed for TalonFX Integrated Sensors
+            iMotor.setInverted(inverted); // Invert Motor?
         }
 
         public static void Index(final TalonSRX iMotor, boolean inverted) {
-            iMotor.configSelectedFeedbackSensor(FeedbackDevice.SoftwareEmulatedSensor); //Set Encoder
-            iMotor.setInverted(inverted); //Set Motor Invert
-            iMotor.configFactoryDefault(); //Use Factory Tune Settings
-            //Output Settings
-            iMotor.configNominalOutputForward(0);
-            iMotor.configNominalOutputReverse(0);
-            iMotor.configPeakOutputForward(1);
-            iMotor.configPeakOutputReverse(-1);
 
-            iMotor.config_kF(Constants.kPIDLoopIdx, Constants.Index.mkF, Constants.kTimeoutMs);
-            iMotor.config_kP(Constants.kPIDLoopIdx, Constants.Index.mkP, Constants.kTimeoutMs);
-            iMotor.config_kI(Constants.kPIDLoopIdx, Constants.Index.mkI, Constants.kTimeoutMs);
-            iMotor.config_kD(Constants.kPIDLoopIdx, Constants.Index.mkD, Constants.kTimeoutMs);
+            iMotor.configFactoryDefault(); //Reset Motor Configuration before applying any kind of user configuration
+            iMotor.configAllSettings(IndexConfiguration()); //Apply new Motor Configuration
 
-            iMotor.setNeutralMode(NeutralMode.Brake); // Set Motor Neutral Mode To Hold
-
-            //iMotor.setSelectedSensorPosition(0); // Reset Motor Encoder
+            //iMotor.setSensorPhase(true); // Sensor Phase (tuning) // Not needed for TalonFX Integrated Sensors
+            iMotor.setInverted(inverted); // Invert Motor?
         }
 
     }
